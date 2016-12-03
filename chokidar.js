b6a79e219fada97ca0736a67f1ad242bc05f8215
Adam Franzen <adamfranzen@gmail.com>
2016-12-03 15:09:47 -0700

    fixed

--- chokidar.js
+++ chokidar.js
@@ -10,9 +10,9 @@ var watcher = chokidar.watch('./pics', {
 var log = console.log.bind(console);
 // Add event listeners.
 watcher
-  .on('add', path => {
-    console.log();
-  }
+  // .on('add', path => {
+  //   console.log();
+  // }
   log(`File ${path} has been added`))
   .on('add', path => log(`File ${path} has been added`))
   .on('change', path => log(`File ${path} has been changed`))
