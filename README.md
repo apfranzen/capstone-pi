227d65f914e3dc17ebb2dcf2c0077106ccf277d4
Adam Franzen <adamfranzen@gmail.com>
2016-12-03 17:30:05 -0700

    links added to README

---
+++ README.md
@@ -0,0 +1,15 @@
+sudo ./rslsync
+~/.btsync
+
+http://10.2.10.156:8888/gui/#
+https://www.resilio.com/blog/sync-wd-raspberry-pi
+sudo ifconfig
+
+didn't work: https://www.resilio.com/blog/sync-hacks-how-to-sync-without-data-loss-using-btsync-raspberry-pi
+
+https://ml.internalpositioning.com/dashboard/precisepointer
+https://doc.internalpositioning.com/api/#get-location
+
+https://www.npmjs.com/package/request-promise
+
+https://www.npmjs.com/package/axios
--- chokidar.js
+++ chokidar.js
@@ -1,4 +1,5 @@
 var chokidar = require('chokidar');
+var rp = require('request-promise');
 
 // Initialize watcher.
 var watcher = chokidar.watch('./pics', {
@@ -10,10 +11,20 @@ var watcher = chokidar.watch('./pics', {
 var log = console.log.bind(console);
 // Add event listeners.
 watcher
-  // .on('add', path => {
-  //   console.log();
-  // }
-  .on('add', path => log(`File ${path} has been added`))
+  .on('add', path => {
+    var options = {
+      uri: 'https://ml.internalpositioning.com/location?group=precisepointer&user=yolo'
+    };
+
+    rp(options)
+      .then(function (findpayload) {
+        console.log(findpayload);
+      })
+      .catch(function (err) {
+        console.log('err: ', err);
+      })
+  })
+  // .on('add', path => log(`File ${path} has been added`))
   .on('add', path => log(`File ${path} has been added`))
   .on('change', path => log(`File ${path} has been changed`))
   .on('unlink', path => log(`File ${path} has been removed`));
